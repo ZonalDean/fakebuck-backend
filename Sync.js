865e16a68616838c8d77acfd5e045c11591e1c55
ZonalDean <huber.deanc@gmail.com>
2022-05-24 16:06:36 +0700

    getMe complete

--- Sync.js
+++ Sync.js
@@ -13,3 +13,8 @@ exports.modDB = (arming) => {
 exports.stringfy = (input) => {
     console.log(JSON.stringify(input, null, 2))
 }
+
+// USE THIS TO PUT OBJECT INTO OBJECT FOR weird objects (SQL is weird setter getter)
+exports.parseStrinfyJSON = (input) => {
+    return JSON.parse(JSON.stringify(input))
+}
--- controller/usersController.js
+++ controller/usersController.js
@@ -1,14 +1,49 @@
 const { User, Friend } = require('../models')
 const createError = require("../utils/createError");
+const friendService = require("../services/friendService")
 
-exports.getMe = async (req, res) => {
+exports.getMe = async (req, res, next) => {
+    try {
 
-    // SEND USER FROM "Authenticate" MIDDLEWARE
-    const user = req.user
-    res.json({ user: user })
+        // HARD WAY
+        // const {
+        //     id,
+        //     firstName,
+        //     lastName,
+        //     email,
+        //     phoneNumber,
+        //     profilePic,
+        //     coverPhoto,
+        //     createdAt,
+        //     updatedAt } = req.user;
 
-    // FIND USER & GET USER DATA
+        // const friends = await friendService.findAcceptedFriends(req.user.id);
 
-    // FIND FRIENDS & GET DATA
+        // res.json({
+        //     user: 
+        //         id,
+        //         firstName,
+        //         lastName,
+        //         email,
+        //         phoneNumber,
+        //         profilePic,
+        //         coverPhoto,
+        //         createdAt,
+        //         updatedAt,
+        //         friends
+        // })
+
+        // COOL WAY
+        const user = JSON.parse(JSON.stringify(req.user)); 
+        const friends = await friendService.findAcceptedFriends(req.user.id);
+
+        user.friends = friends;
+
+        res.json({ user });
+
+    } catch (err) {
+        next(err)
+    }
+
 
 }
